9a56a00c481c3150c00210c29c27c2e9f48ea00e
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-08-17 17:38:09 +0200

    basic init

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const host = 'localhost';
 const port = 8080;
 
 // Body parser middelware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Hello World!');
