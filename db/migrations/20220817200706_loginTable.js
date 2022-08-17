d8244c151eab89925634cd6970ff8247736ea139
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-08-18 01:37:46 +0200

    setup db connection with knex and create tables

---
+++ db/migrations/20220817200706_loginTable.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+	return knex.schema.createTable('login', table => {
+		table.increments('id').primary();
+		table.string('hash', 100).notNullable();
+		table.text('email').unique().notNullable();
+	});
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+	return knex.schema.dropTable('login');
+};
